0d6ab4bd3add0449e25885e60564b662c503db86
Debora da Silva <deboradasilva@pt-330333-mbp16m1.lan>
2023-08-11 09:59:15 +0100

    test: Creating Login Page Object

---
+++ Resources/PageObject/Locators/SauceDemoInventoryLocators.py
@@ -0,0 +1,12 @@
+
+# Sauce Demo Inventory Page
+InventoryContentDiv = "id:contents_wrapper"
+
+# Sauce Demo Inventory Actions
+InventoryBurgerMenuButton = "id:react-burger-menu-btn"
+InventoryShoppingCartDiv = "id:shopping_cart_container"
+InventoryProductSortSelect = "data-test:product_sort_container"
+
+# Sauce Demo Inventory Container
+InventoryContainerDiv = "id:inventory_container"
+
--- Resources/PageObject/Locators/SauceDemoLocators.py
+++
@@ -1,4 +0,0 @@
-# Sauce Demo Login Page
-LoginUsernameInputText = "data-test:username"
-LoginPasswordInputText = "data-test:password"
-LoginSubmitButton = "data-test:login-button"
