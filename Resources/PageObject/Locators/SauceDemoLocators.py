91a1107fe555d13a80d80ffb86a613a61cf3779d
Debora da Silva <deboradasilva@pt-330333-mbp16m1.lan>
2023-08-11 09:34:02 +0100

    test: Creating Login Page Object

---
+++ Resources/PageObject/Locators/SauceDemoLocators.py
@@ -0,0 +1,4 @@
+# Sauce Demo Login Page
+LoginUsernameInputText = "data-test:username"
+LoginPasswordInputText = "data-test:password"
+LoginSubmitButton = "data-test:login-button"
---
+++ Resources/PageObject/TestData/SauceDemoData.py
@@ -0,0 +1,3 @@
+# Standard user login test data
+StandardUsername = "standard_user"
+StandardPassword = "secret_sauce"
